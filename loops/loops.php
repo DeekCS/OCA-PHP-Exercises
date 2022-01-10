a5d44805fc133f9e06a67d02c4dbb8010d8b9fe8
DeekCS <31720001079@std.bau.edu.jo>
2022-01-11 00:11:48 +0200

    loops

---
+++ loops/loops.php
@@ -0,0 +1,35 @@
+<?php
+
+#1. Create Script that displays 1-2-3-4-5-6-7-8-9-10 on one line,
+# there will not be dash(-) before first number and after last number.
+/*
+for ($i = 1; $i <= 10; $i++) {
+    echo $i;
+    if ($i < 10) {
+        echo "-";
+    }
+}
+*/
+
+#2. Create a script that displays all numbers between 1 and 30 with total sum.
+/*
+    $sum =0;
+for ($i = 1; $i <= 30; $i++) {
+    $sum += $i;
+    echo $i . " ";
+}
+echo "<br>";
+    echo "The Sum is: " . $sum;
+*/
+
+#3. Script a script to generate the following pattern.
+
+//A A A A A
+//A A A B B
+//A A C C C
+//A D D D D
+//E E E E E
+
+
+
+
