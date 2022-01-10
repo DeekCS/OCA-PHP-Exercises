5ee61985d5c4001193347549e09e677fcfe79774
DeekCS <31720001079@std.bau.edu.jo>
2022-01-10 10:34:48 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+#Qustion 1:
+##1.1 Convert the entered string to upper case.
+/*
+function upper_case($str)
+{
+    return strtoupper($str);
+}
+
+echo(upper_case("hi world"));
+*/
+
+##1.2 Convert the entered string to lower case.
+/*
+ * function lowe_case($str)
+ * {
+ * return strtolower($str);
+ * }
+ * echo(lowe_case("HI WORLD"));
+ */
+
+##1.3 Make the first letter of the entered string capital.
+
+function first_letter_uppercase() {
+
+}
