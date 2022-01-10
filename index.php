e26608cfe03347fe7a3e2d438d48a1f3be7144f4
DeekCS <31720001079@std.bau.edu.jo>
2022-01-10 10:39:36 +0200

    Q1 Done

--- index.php
+++ index.php
@@ -12,15 +12,31 @@ echo(upper_case("hi world"));
 
 ##1.2 Convert the entered string to lower case.
 /*
- * function lowe_case($str)
- * {
- * return strtolower($str);
- * }
- * echo(lowe_case("HI WORLD"));
+  function lowe_case($str)
+  {
+  return strtolower($str);
+  }
+  echo(lowe_case("HI WORLD"));
  */
 
 ##1.3 Make the first letter of the entered string capital.
 
-function first_letter_uppercase() {
-
+/*
+function first_letter_uppercase($str) {
+    return ucfirst($str);
 }
+
+echo first_letter_uppercase("hello world");
+
+*/
+
+##1.4 Make the first letter of each word in the entered string capital.
+
+/*
+function first_letter_uppercase_each_word($str) {
+    return ucwords($str);
+}
+
+echo first_letter_uppercase_each_word("hello world");
+
+*/
